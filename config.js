e3c2d7e3d607b50817dafeb44e8279c704fa5e92
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-01-31 07:32:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -68,8 +68,8 @@ module.exports = kconfig = async (kill, message) => {
         const groupAdmins = isGroupMsg ? await kill.getGroupAdmins(groupId) : ''
         const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
         const isBotGroupAdmins = isGroupMsg ? groupAdmins.includes(botNumber + '@c.us') : false
-		const chats = (type === 'chat') ? body : (type === 'image' || type === 'video') ? caption : ''
-        const ownerNumber = '529984907794' // MUDE ISSO PARA O SEU NUMERO
+	const chats = (type === 'chat') ? body : (type === 'image' || type === 'video') ? caption : ''
+        const ownerNumber = '9984907794' // MUDE ISSO PARA O SEU NUMERO
         const isOwner = sender.id === ownerNumber
         global.pollfile = 'poll_Config_'+chat.id+'.json'
         global.voterslistfile = 'poll_voters_Config_'+chat.id+'.json'
@@ -213,6 +213,14 @@ module.exports = kconfig = async (kill, message) => {
 		case 'ttp':
 			if (args.length == 0) return kill.reply(from, '¿Dónde está la frase?', id)
 			axios.get(`https://st4rz.herokuapp.com/api/ttp?kata=${body.slice(5)}`)
+			.then(res => {
+				kill.sendImageAsSticker(from, res.data.result)
+			})
+			break
+			
+		case 'attp':
+			if (args.length == 0) return kill.reply(from, '¿Dónde está la frase?', id)
+			axios.get(`https://api.xteam.xyz/attp?file&text=${body.slice(5)}`)
 			.then(res => {
 				kill.sendImageAsSticker(from, res.data.result)
 			})
@@ -1146,20 +1154,73 @@ if (isMedia) {
 				await kill.reply(from, 'Este comando solo se puede usar en grupos!', id)
 			}
             break
+			
+	case 'lirik':
+                    if (args.length < 1) return reply('donde esta el titulo de la cancion, tio🤨')
+                    teha = body.slice(7)
+                    anu = await fetchJson(`https://arugaz.herokuapp.com/api/lirik?judul=${body.slice(5)}` , {method: 'get'})
+                    reply(anu.result)
+                break
 
 
-	case 'type':
-            if (args.length !== 0) return kill.reply(from, 'Olvidaste ponerlo(ON) o quitarlo [off].', id)
-			if (args[0] == 'on') {
-				kill.simulateTyping(from, true, id)
-                kill.reply(from, '¡Hecho! Se ha activado la simulacion de "Escribiendo".', id)
-			} else if (args[0] == 'off') {
-				kill.simulateTyping(from, false, id)
-                kill.reply(from, 'Entendido! Desactive las opciones de simulacion "Escribiendo"', id)
-            } else {
-                kill.reply(from, 'Olvidaste ponerlo (ON) o quitarlo [off].', id)
-            }
-            break
+			case 'stiker': 
+				case 'sticker':
+				case 's':
+					if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+						const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+						const media = await kill.downloadAndSaveMediaMessage(encmedia)
+						ran = getRandom('.webp')
+						await ffmpeg(`./${media}`)
+							.input(media)
+							.on('start', function (cmd) {
+								console.log(`Started : ${cmd}`)
+							})
+							.on('error', function (err) {
+								console.log(`Error : ${err}`)
+								fs.unlinkSync(media)
+								kill.reply(mess.error.stick)
+							})
+							.on('end', function () {
+								console.log('Finish')
+								buff = fs.readFileSync(ran)
+								kill.sendMessage(from, buff, sticker, {quoted: mek})
+								fs.unlinkSync(media)
+								fs.unlinkSync(ran)
+							})
+							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+							.toFormat('webp')
+							.save(ran)
+					} else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+						const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+						const media = await client.downloadAndSaveMediaMessage(encmedia)
+						ran = getRandom('.webp')
+						kill.reply(mess.wait)
+						await ffmpeg(`./${media}`)
+							.inputFormat(media.split('.')[1])
+							.on('start', function (cmd) {
+								console.log(`Started : ${cmd}`)
+							})
+							.on('error', function (err) {
+								console.log(`Error : ${err}`)
+								fs.unlinkSync(media)
+								tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+								kill.reply(`A fallado ;(, intenta de nuevo ^_^`)
+							})
+							.on('end', function () {
+								console.log('Finish')
+								buff = fs.readFileSync(ran)
+								kill.sendMessage(from, buff, sticker, {quoted: mek})
+								fs.unlinkSync(media)
+								fs.unlinkSync(ran)
+							})
+							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+							.toFormat('webp')
+							.save(ran)
+							} else {
+						kill.reply(`Enviar la imagen con la etiqueta ${prefix} o */tag* imagen`)
+					}
+					break
+			
 			
         case 'welcome':
 			if (!isGroupMsg) return kill.reply(from, mess.error.Gp, id)
@@ -1172,7 +1233,9 @@ if (isMedia) {
 				let welcom = welcome.indexOf(chatId)
                 welkom.splice(welcom, 1)
                 fs.writeFileSync('./lib/welcome.json', JSON.stringify(welkom))
+				kill.simulateTyping(from, true, id)
                 kill.reply(from, 'Entendido! Desactive las opciones de Bienvenida y Adiós.', id)
+				kill.simulateTyping(from, true, id)
             } else {
                 kill.reply(from, 'Olvidaste ponerlo (ON) o quitarlo [off].', id)
             }
